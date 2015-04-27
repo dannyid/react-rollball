839c7733de6d29c1caf51911c427053b9b87b318
dannyid <danielshekhtman@gmail.com>
2015-04-27 10:23:28 -0400

    Add some bootstrap. (Still broken looking)

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import Header from './components/header';
+import Body from './components/body';
 
 class App extends React.Component {
   render() {
     return (
-      <Header name="Danny"/>
+      <div id="react-rollball">
+        <Header/>
+        <Body name="Danny"/>
+      </div>
     );
   }
 }
 
 React.render(
-  <App />,
+  <App/>,
   document.body
 );
--- src/jsx/components/header.jsx
+++ src/jsx/components/header.jsx
@@ -1,9 +1,22 @@
-/*jshint esnext: true */
-
 import React from 'react';
  
 export default class Header extends React.Component {
   render() {
-    return <div className='page-header'>Hello {this.props.name}</div>;
+    return (
+      <nav className='navbar navbar-inverse navbar-fixed-top'>
+        <div className="container">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">Project name</a>
+          </div>
+          <div id="navbar" className="collapse navbar-collapse">
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
   }
 };
